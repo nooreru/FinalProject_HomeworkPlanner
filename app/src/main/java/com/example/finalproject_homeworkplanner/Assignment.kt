4a396f2006a83c0a4d56eb8de5ec341d33982fae
nooreru <ruttyn24@hsnet.ahsd.org>
2023-04-24 10:38:30 -0400

    4/24/23

---
+++ app/src/main/java/com/example/finalproject_homeworkplanner/Assignment.kt
@@ -0,0 +1,4 @@
+package com.example.finalproject_homeworkplanner
+
+data class Assignment (var assignmentCourse: String, var assignmentName: String, var assignmentDueDate: Integer){
+}
---
+++ app/src/main/java/com/example/finalproject_homeworkplanner/HomeScreenFragment.kt
@@ -0,0 +1,5 @@
+package com.example.finalproject_homeworkplanner
+
+class HomeScreenFragment {
+
+}
